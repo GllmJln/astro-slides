23b2c43c6339a19710744c8702767c29d1b64bd8
GllmJln <gjuliens10@gmail.com>
2024-03-26 23:26:07 +0000

    initial commit

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// Define a `type` and `schema` for each collection
+const slideCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    backgroundColor: z.string(),
+    align: z.string().optional(),
+    output: z.string().optional(),
+  }),
+});
+
+// Export a single `collections` object to register your collection(s)
+export const collections = {
+  slides: slideCollection,
+};
